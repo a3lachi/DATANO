29355840a56daf97bc2432d8628b744ac347d5a4
a3lachi <mad.aalachi@gmail.com>
2023-03-06 21:19:47 +0100

    code update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -153,7 +153,7 @@ class App extends Component{
 		var url = '{{width: 200}}'
 		if(currInstru.src){
 			return(
-				<canvas id="kanva" style={{background: 'url('+ currInstru.src.toString() + ')' , width='300px'}} >
+				<canvas id="kanva" style={{background: 'url('+ currInstru.src.toString() + ')' , backgroundSize:'100% 100%'}} >
 					<div className="row border">
 
 		              	<div className="col border" id="lol"  style={{position: 'relative'}}  onClick={this.cropCenter} ><img id="mainimage" alt={currInstru.taskId} src={currInstru.src} onDrag={this.dragView}/></div>
