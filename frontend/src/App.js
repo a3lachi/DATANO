68a3f13555a4be5739d56ab84b8a9169988b81fa
a3lachi <mad.aalachi@gmail.com>
2023-03-06 03:46:25 +0100

    code update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -126,40 +126,43 @@ class Left extends Component{
           
           <div class="row border">
 
-          <div class="col-lg-2 border">
+	          <div class="col-lg-2 border">
 
 
-            <div class="col border">
-              <div class="row border">
-                  Objects :
-                  
-              </div>
+	          	<Left data="" ></Left>
 
-              <div class="row border">
-                <div class="col border">
-                    { firstCollecArray.map((item,index) => { return <div class="row"><input type="checkbox" name="myCheckbox" id={item} onClick={this.selectOnlyThis} /><label>{item}</label></div>; }) }
-                    { restCollec.map((item,index) => { return <div class="row"><input type="checkbox" name="myCheckbox" id={item} onClick={this.selectOnlyThis} /><label>{item}</label></div>; }) }
-                </div>
-              </div>
-            </div>
 
-            <div class="col" style={{height : '40px'}}></div>
+	            <div class="col border">
+	              <div class="row border">
+	                  Objects :
+	                  
+	              </div>
 
-            <div class="col border">
-                <div class="row border">
-                  Up Next 
-                </div>
-                <div class="row border">
-                  
-                  <div class="col border">
-                    {viewCollec.map((item,index) => { return <div class="row"><img src={item.src} style={{width:'100%'}} /><button name="buttonImg" id={item.id} onClick={this.chooseInstruction}>{item.taskId}</button></div>; }) }
-                  </div>
-                  
-                </div>
-            </div>
-            
-            
-          </div>
+	              <div class="row border">
+	                <div class="col border">
+	                    { firstCollecArray.map((item,index) => { return <div class="row"><input type="checkbox" name="myCheckbox" id={item} onClick={this.selectOnlyThis} /><label>{item}</label></div>; }) }
+	                    { restCollec.map((item,index) => { return <div class="row"><input type="checkbox" name="myCheckbox" id={item} onClick={this.selectOnlyThis} /><label>{item}</label></div>; }) }
+	                </div>
+	              </div>
+	            </div>
+
+	            <div class="col" style={{height : '40px'}}></div>
+
+	            <div class="col border">
+	                <div class="row border">
+	                  Up Next 
+	                </div>
+	                <div class="row border">
+	                  
+	                  <div class="col border">
+	                    {viewCollec.map((item,index) => { return <div class="row"><img src={item.src} style={{width:'100%'}} /><button name="buttonImg" id={item.id} onClick={this.chooseInstruction}>{item.taskId}</button></div>; }) }
+	                  </div>
+	                  
+	                </div>
+	            </div>
+	            
+	            
+	          </div>
           
           
           <div class="col-lg-7 border">
