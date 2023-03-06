bf2e80b5fc7df118e746ede5397b2f4bd32ecc94
a3lachi <mad.aalachi@gmail.com>
2023-03-06 04:01:33 +0100

    code update

--- frontend/src/components/Left.js
+++ frontend/src/components/Left.js
@@ -3,77 +3,62 @@ import React, { Component } from "react";
 
 
 
-class Left extends Component {
+function Left(props) {
 
-    constructor(props){
-      super(props);
-      this.state = {
-        firstCollecArray: props.data[0],
-        restCollec: props.data[1],
-        viewCollec:props.data[2],
-        currentInstru:props.data[3]
-      }
+    const data = props.data 
+    var firstCollecArray: data[0]
+    var restCollec: data[1]
+    var viewCollec: data[2]
+    var currentInstru: data[3]
 
-    }
+    console.log("Data passed left  ", data)
 
-    componentDidMount() {
-      this.refreshList();
-    }
+    return(
 
-    refreshList(){
-      this.setState({ firstCollecArray: props.data[0] })
-    }
+            <div class="col-lg-2 border">
 
 
-    render(){
-      console.log("Firstcollec  ",this.state.firstCollecArray)
-
-      return(
-
-              <div class="col-lg-2 border">
+              <Left data="" ></Left>
 
 
-                <Left data="" ></Left>
-
-
-                <div class="col border">
-                  <div class="row border">
-                      Objects :
-                      
-                  </div>
-
-                  <div class="row border">
-                    <div class="col border">
-                        { this.firstCollecArray.map((item,index) => { return <div class="row"><input type="checkbox" name="myCheckbox" id={item} onClick={this.selectOnlyThis} /><label>{item}</label></div>; }) }
-                        { this.estCollec.map((item,index) => { return <div class="row"><input type="checkbox" name="myCheckbox" id={item} onClick={this.selectOnlyThis} /><label>{item}</label></div>; }) }
-                    </div>
-                  </div>
+              <div class="col border">
+                <div class="row border">
+                    Objects :
+                    
                 </div>
 
-                <div class="col" style={{height : '40px'}}></div>
-
-                <div class="col border">
-                    <div class="row border">
-                      Up Next 
-                    </div>
-                    <div class="row border">
-                      
-                      <div class="col border">
-                        {this.viewCollec.map((item,index) => { return <div class="row"><img src={item.src} style={{width:'100%'}} /><button name="buttonImg" id={item.id} onClick={this.chooseInstruction}>{item.taskId}</button></div>; }) }
-                      </div>
-                      
-                    </div>
+                <div class="row border">
+                  <div class="col border">
+                      { this.firstCollecArray.map((item,index) => { return <div class="row"><input type="checkbox" name="myCheckbox" id={item} onClick={this.selectOnlyThis} /><label>{item}</label></div>; }) }
+                      { this.estCollec.map((item,index) => { return <div class="row"><input type="checkbox" name="myCheckbox" id={item} onClick={this.selectOnlyThis} /><label>{item}</label></div>; }) }
+                  </div>
                 </div>
-                
-                
               </div>
 
+              <div class="col" style={{height : '40px'}}></div>
+
+              <div class="col border">
+                  <div class="row border">
+                    Up Next 
+                  </div>
+                  <div class="row border">
+                    
+                    <div class="col border">
+                      {this.viewCollec.map((item,index) => { return <div class="row"><img src={item.src} style={{width:'100%'}} /><button name="buttonImg" id={item.id} onClick={this.chooseInstruction}>{item.taskId}</button></div>; }) }
+                    </div>
+                    
+                  </div>
+              </div>
+              
+              
+            </div>
 
 
 
 
-      )
-    }
+
+    )
+    
 }
 
 
