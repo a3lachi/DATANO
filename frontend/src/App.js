d678beeb972897c62cd5e1b2f01c35b87127ad55
a3lachi <mad.aalachi@gmail.com>
2023-03-06 04:06:36 +0100

    code update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -71,6 +71,64 @@ class App extends Component{
       this.refreshList();
     }
 
+    Left(props) {
+
+	    const data = props
+	    var firstCollecArray: data[0]
+	    var restCollec: data[1]
+	    var viewCollec: data[2]
+	    var currentInstru: data[3]
+
+	    console.log("Data passed left  ", props)
+
+	    return(
+
+	            <div class="col-lg-2 border">
+
+
+	              <Left data="" ></Left>
+
+
+	              <div class="col border">
+	                <div class="row border">
+	                    Objects :
+	                    
+	                </div>
+
+	                <div class="row border">
+	                  <div class="col border">
+	                      { firstCollecArray.map((item,index) => { return <div class="row"><input type="checkbox" name="myCheckbox" id={item} onClick={this.selectOnlyThis} /><label>{item}</label></div>; }) }
+	                      { restCollec.map((item,index) => { return <div class="row"><input type="checkbox" name="myCheckbox" id={item} onClick={this.selectOnlyThis} /><label>{item}</label></div>; }) }
+	                  </div>
+	                </div>
+	              </div>
+
+	              <div class="col" style={{height : '40px'}}></div>
+
+	              <div class="col border">
+	                  <div class="row border">
+	                    Up Next 
+	                  </div>
+	                  <div class="row border">
+	                    
+	                    <div class="col border">
+	                      {viewCollec.map((item,index) => { return <div class="row"><img src={item.src} style={{width:'100%'}} /><button name="buttonImg" id={item.id} onClick={this.chooseInstruction}>{item.taskId}</button></div>; }) }
+	                    </div>
+	                    
+	                  </div>
+	              </div>
+	              
+	              
+	            </div>
+
+
+
+
+
+	    )
+	    
+	}
+
 
     refreshList = () => {
       axios
@@ -128,7 +186,7 @@ class App extends Component{
           
           <div class="row border">
 
-          		<Left data={dataCollection}></Left>
+          		{this.Left(dataCollection)}
 	          {/*<div class="col-lg-2 border">
 
 
