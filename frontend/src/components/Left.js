189b0eb09815ff1085810f3fc23658d0a9b5701e
a3lachi <mad.aalachi@gmail.com>
2023-03-06 04:26:46 +0100

    code update

--- frontend/src/components/Left.js
+++ frontend/src/components/Left.js
@@ -3,59 +3,62 @@ import React, { Component } from "react";
 
 
 
-function Left(props) {
-
-    const data = props.data 
-    var firstCollecArray: data[0]
-    var restCollec: data[1]
-    var viewCollec: data[2]
-    var currentInstru: data[3]
-
-    console.log("Data passed left  ", props)
-
-    return(
-
-            <div class="col-lg-2 border">
+class Left extends Component {
 
 
-              <div class="col border">
-                <div class="row border">
-                    Objects :
-                    
-                </div>
+    render() {
+      console.log("Data passed left  ", this.props)
+      const data = this.props.data
+      var firstCollecArray: data[0]
+      var restCollec: data[1]
+      var viewCollec: data[2]
+      var currentInstru: data[3]
 
-                <div class="row border">
-                  <div class="col border">
-                      { this.firstCollecArray.map((item,index) => { return <div class="row"><input type="checkbox" name="myCheckbox" id={item} onClick={this.selectOnlyThis} /><label>{item}</label></div>; }) }
-                      { this.estCollec.map((item,index) => { return <div class="row"><input type="checkbox" name="myCheckbox" id={item} onClick={this.selectOnlyThis} /><label>{item}</label></div>; }) }
-                  </div>
-                </div>
-              </div>
 
-              <div class="col" style={{height : '40px'}}></div>
+      return(
 
-              <div class="col border">
+              <div class="col-lg-2 border">
+
+
+                <div class="col border">
                   <div class="row border">
-                    Up Next 
+                      Objects :
+                      
                   </div>
+
                   <div class="row border">
-                    
                     <div class="col border">
-                      {this.viewCollec.map((item,index) => { return <div class="row"><img src={item.src} style={{width:'100%'}} /><button name="buttonImg" id={item.id} onClick={this.chooseInstruction}>{item.taskId}</button></div>; }) }
+                        { this.firstCollecArray.map((item,index) => { return <div class="row"><input type="checkbox" name="myCheckbox" id={item} onClick={this.selectOnlyThis} /><label>{item}</label></div>; }) }
+                        { this.estCollec.map((item,index) => { return <div class="row"><input type="checkbox" name="myCheckbox" id={item} onClick={this.selectOnlyThis} /><label>{item}</label></div>; }) }
                     </div>
-                    
                   </div>
+                </div>
+
+                <div class="col" style={{height : '40px'}}></div>
+
+                <div class="col border">
+                    <div class="row border">
+                      Up Next 
+                    </div>
+                    <div class="row border">
+                      
+                      <div class="col border">
+                        {this.viewCollec.map((item,index) => { return <div class="row"><img src={item.src} style={{width:'100%'}} /><button name="buttonImg" id={item.id} onClick={this.chooseInstruction}>{item.taskId}</button></div>; }) }
+                      </div>
+                      
+                    </div>
+                </div>
+                
+                
               </div>
-              
-              
-            </div>
 
 
 
 
 
-    )
-    
+      )
+   }
+      
 }
 
 
