1c4576664533ace1071dfac6012fc85d78283a26
a3lachi <mad.aalachi@gmail.com>
2023-03-07 02:56:23 +0100

    code update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -167,8 +167,8 @@ class App extends Component{
 	qsstiKanvaDown(event){
 		var nva = document.querySelector('canvas')
 		
-		this.canvaCordX = event.pageX
-		this.canvaCordY = event.pageY
+		this.canvaCordX = event.clientX
+		this.canvaCordY = event.clientY
 
 		console.log('Qssti kanva ', event.screenX , event.screenY)
 		this.isCrop = 1
@@ -180,7 +180,7 @@ class App extends Component{
 			var context = canvas.getContext("2d");
 
 
-			context.rect(this.canvaCordX-285 , this.canvaCordY-193 , event.pageX-this.canvaCordX, event.pageY-this.canvaCordY);
+			context.rect(this.canvaCordX-285 , this.canvaCordY-193 , event.clientX-this.canvaCordX, event.clientY-this.canvaCordY);
 			context.globalAlpha = 0.3
 			context.fillStyle = "#FF0000";
 			context.fill();
