6793b2f65ea525683f13aae573bb48286821079b
a3lachi <mad.aalachi@gmail.com>
2023-03-07 01:20:39 +0100

    code update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -191,8 +191,9 @@ class App extends Component{
 		if (this.isCrop == 1) {
 			var canvas = document.querySelector('canvas');
 			var context = canvas.getContext("2d");
+			context.clearRect(0, 0, canvas.width, canvas.height);
 
-			context.rect(this.canvaCordX , this.canvaCordY , event.clientX, event.clientY);
+			context.rect(this.canvaCordX , this.canvaCordY , Math.abs(event.clientX-this.canvaCordX), Math.abs(event.clientY-this.canvaCordY));
 			context.globalAlpha = 0.2
 			context.fillStyle = "#8ED6FF";
 			context.fill();
