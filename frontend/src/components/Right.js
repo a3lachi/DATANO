8848e60b3c2ccf2e73474d80d9a40a3f6e36f82d
a3lachi <mad.aalachi@gmail.com>
2023-03-10 06:12:38 +0100

    code update

--- frontend/src/components/Right.js
+++ frontend/src/components/Right.js
@@ -5,7 +5,61 @@ import { canvas } from "canvas";
 
 class Right extends Component{
 
+    render(){
+    var currentInstru = this.props.currentInstru
 
+    if(currentInstru.instru) {
+        
+            return(
+                <div className="col-lg-3">
+                    
+                    <div id="infoz" className="col">
+                        <div className="row">
+                            Info 
+                        </div>
+                        <div className="row">
+                            <div className="col">
+                                <div className="row">
+                                    Instruction : {currentInstru.instru}
+                                </div>
+                                <div className="row">
+                                    Task ID : {currentInstru.taskId}
+                                </div>
+                                <div className="row">
+                                    Created At : {currentInstru.createdAt}
+                                </div>
+                                <div className="row">
+                                    Urgency : {currentInstru.urgency}
+                                </div>
+                                <div className="row">
+                                    Original Image : 
+                                </div>
+                            </div>
+                        </div>
+                        
+                        
+                    </div>
+
+
+                    <div className="col" style={{height : '20px'}}></div>
+                    
+
+                    <div id="zcrops" className="col">
+                        <div className="row" style={{height : '30px'}}>Annotations</div>
+                        <div className="row" style={{height : '30px'}}></div>
+                        <div id="cropat" className="col" style={{height:'450px' , overflow : 'auto'}} >
+                            
+                        </div>
+
+                        <div className="row" style={{height : '200px'}}>
+                            
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+    }
+    else { return null}
 
 }
 
